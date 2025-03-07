4edda2b3e43a50a33db08b8c7c0b9818357e909d
dpaz52418 <dpaz52418@gmail.com>
2025-03-07 13:23:42 -0800

    Understanding APIs, Temporary Tests
    New Branch. Successfully connected backend to databases on MongoDB. Created .env file not committed here. Currently experimenting with APIs to get an understanding before extending application to relevant app data.

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,54 @@
 const express = require('express')
-const app = express()
+const mongoose = require('mongoose');
+require('dotenv').config();
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
-});
+const Product = require('./models/product.model.js');
+
+const app = express();
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.send("Hello from Node API")
+    res.send("Hello from Node API Updated 2");
 });
+
+app.get('/api/products', async (req, res) => {
+
+    try {
+        const products = await Product.find({});
+        res.status(200).json(products);
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+});
+
+app.get('/api/product/:id', async (req, res) => {
+
+    try {
+        const { id } = req.params;
+        const product = await Product.findById(id);
+        res.status(200).json(product);
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+});
+
+app.post('/api/products', async (req, res) => {
+    try {
+        const product = await Product.create(req.body);
+        res.status(200).json(product);
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+});
+
+mongoose.connect(process.env.MONGODB_URI)
+.then(() => {
+    console.log("Connected to database!");
+    app.listen(3000, () => {
+        console.log('Server is running on port 3000');
+    });
+})
+.catch(() => {
+    console.log("Connection failed...");
+})

81ee58a25bdcfa8fc0371f95de0589c5bdb3d76b
dpaz52418 <dpaz52418@gmail.com>
2025-03-11 15:22:52 -0700

    CORS Configuration Prep + Port 5000
    Started preparing CORS configuration to prepare to link backend with frontend. Also changed to ensure backend runs on port 5000 instead of 3000.

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 require('dotenv').config();
 
 // A formality. I just like them being here to know where we are.
@@ -15,6 +16,12 @@ const authRoutes = require('./routes/auth.route.js');
 const app = express();
 
 // Middleware Configuration
+app.use(cors({
+    origin: 'http://localhost:3000', // Frontend 
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true // Allow cookies if you need them
+}));
 app.use(express.json());
 
 // Add routes
@@ -31,8 +38,8 @@ app.get('/', (req, res) => {
 mongoose.connect(process.env.MONGODB_URI)
 .then(() => {
     console.log("Connected to database!");
-    app.listen(3000, () => {
-        console.log('Server is running on port 3000');
+    app.listen(5000, () => {
+        console.log('Server is running on port 5000');
     });
 })
 .catch(() => {
